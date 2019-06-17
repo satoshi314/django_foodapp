3cd9b6bd80f83aa9c90be835f12c4470e96ea03d
satoshi314 <satosi_aa@yahoo.co.jp>
2019-06-17 23:05:07 +0900

    駅検索修正

--- app/views.py
+++ app/views.py
@@ -88,7 +88,7 @@ def mylist_search(request):
         if name :    #空でないとき
             shops = Shop.objects.filter(name__icontains=name)  #_は２つ！
         if station :    #空でないとき
-            shops = Shop.objects.filter(station__icontains=name)            
+            shops = Shop.objects.filter(station__icontains=staion)            
         if genre   :
             shops = Shop.objects.filter(genre__icontains=genre)   
         if shops :   #検索結果が空かどうかの判断 
