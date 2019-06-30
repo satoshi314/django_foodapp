d313e4bffcca79fa721749a1667954e10279d1aa
satoshi314 <satosi_aa@yahoo.co.jp>
2019-06-30 16:35:52 +0900

    static root

--- MyFood/settings.py
+++ MyFood/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))      #BASE_DIRとおなじ
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''
 STATIC_URL =  '/static/'
 
 STATICFILES_DIRS = (
