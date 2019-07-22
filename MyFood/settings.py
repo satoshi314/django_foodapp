95e9361693fc1b5bf2e555d90d40d04bb3bb39af
satoshi314 <satosi_aa@yahoo.co.jp>
2019-07-22 17:07:30 +0900

    strage off

--- MyFood/settings.py
+++ MyFood/settings.py
@@ -145,7 +145,7 @@ STATICFILES_FINDERS = (
      'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' 
+#STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 
 
