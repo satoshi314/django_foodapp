31b20abe349fe8a0f73f32173dacab5a82fbd85b
satoshi314 <satosi_aa@yahoo.co.jp>
2019-09-08 21:19:00 +0900

    fix css

--- MyFood/wsgi.py
+++ MyFood/wsgi.py
@@ -14,8 +14,8 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFood.settings')
 
-#application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+# application = Cling(get_wsgi_application())
 
 #whitenoise用の古い設定 最新版ではこのファイルでの設定不要？
 #application = get_wsgi_application()
