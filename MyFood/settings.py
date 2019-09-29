850db8b78c3c23ab06c2d17544f7db802ec81620
satoshi314 <satosi_aa@yahoo.co.jp>
2019-09-29 21:01:25 +0900

    debug off

--- MyFood/settings.py
+++ MyFood/settings.py
@@ -167,7 +167,7 @@ LOGIN_REDIRECT_URL = 'app:index'
 LOGOUT_REDIRECT_URL = 'app:index'
 
 
-DEBUG = True
+DEBUG = False
 #DEBUG = True    #�e�X�g�p�̈ꎞ�Ή��˖{�ԂɃf�v���C����ƃG���[�ɂȂ�
 try:
     from .local_settings import *
