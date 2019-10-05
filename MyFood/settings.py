68dd77b502dd082ecc1866d9559e56773f44665b
satoshi314 <satosi_aa@yahoo.co.jp>
2019-10-05 16:46:53 +0900

    static

--- MyFood/settings.py
+++ MyFood/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 #manage.py collectstaticを実行した時に、staticファイルが出力されるパス
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #Djangoテンプレートが読みに行く場所?
 #static 固定でよい
@@ -147,7 +147,7 @@ STATIC_URL =  '/static/'
 #FileSystemFinder でファイルの検索先となるディレクトリを指定
 #デフォルトではapp配下のみ
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app/static'),
 )
 
 
